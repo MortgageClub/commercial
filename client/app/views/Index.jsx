12daa0d5c70917c4168e72e38b79e8df0b6d439a
Tang Nguyen <nvtang.khtn@gmail.com>
2016-12-05 17:33:11 +0700

    add reducers

--- client/app/views/Index.jsx
+++ client/app/views/Index.jsx
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 
 class Index extends Component {
   render() {
-    var test1 = "123";
-    const test = <h1>Hello {test1}</h1>;
     return (
       <div className="container">
-        {test}
         {this.props.children}
       </div>
     )
