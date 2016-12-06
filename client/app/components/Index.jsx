e49ddd5ddfc9f62a6ef69187a4bc77ae2bd41f26
Tang Nguyen <nvtang.khtn@gmail.com>
2016-12-06 12:54:49 +0700

    update UI part 1

--- client/app/components/Index.jsx
+++ client/app/components/Index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './Home';
 
 class Index extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
+        <Home />
         {this.props.children}
       </div>
     )
--- client/app/routes.jsx
+++ client/app/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import Index from './views/Index'
+import Index from './components/Index'
 
 export default (
   <Route path="/" component={Index}>
