034c920e514787714b1be9f1d284c3c051abb841
Tang Nguyen <nvtang.khtn@gmail.com>
2016-12-19 13:45:29 +0700

    add admin login

--- app/controllers/advisors/base_controller.rb
+++ app/controllers/advisors/base_controller.rb
@@ -1,4 +1,30 @@
 class Advisors::BaseController < ActionController::Base
   layout "advisor"
   protect_from_forgery with: :exception
+
+  before_action :require_login, :authorize
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    current_user != nil
+  end
+
+  def require_login
+    unless signed_in?
+      flash[:notice] = "You must sign in to see this page"
+      redirect_to sessions_new_path
+    end
+  end
+
+  def authorize
+    unless current_user.subjectable_type == "LoanMember"
+      session[:user_id] = nil
+      flash[:notice] = "You have no right to access this page"
+      redirect_to sessions_new_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
   def confirmation_required?
     false
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
