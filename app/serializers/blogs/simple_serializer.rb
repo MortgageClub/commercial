3978c82535b7c895408d978912dea07ab1f515b1
Tang Nguyen <nvtang.khtn@gmail.com>
2016-12-19 18:03:58 +0700

    update image url: use thumb url

--- app/serializers/blogs/simple_serializer.rb
+++ app/serializers/blogs/simple_serializer.rb
@@ -3,7 +3,7 @@ module Blogs
     attributes :title, :short_description, :image_url, :id, :slug
 
     def image_url
-      object.image.present? ? object.image.expiring_url(60) : ""
+      object.image.present? ? object.image.expiring_url(60, :thumb) : ""
     end
   end
 end
