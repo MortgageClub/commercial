7ad8582cd987bab675cfaa10d91a032d75747d15
Tang Nguyen <nvtang.khtn@gmail.com>
2017-01-11 12:12:20 +0700

    update image

--- client/app/components/pages/borrower/referral/_ReferralLink.jsx
+++ client/app/components/pages/borrower/referral/_ReferralLink.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ClipboardButton from 'react-clipboard.js';
 
 class ReferralLink extends Component {
   render() {
     const link = this.props.userInfo ? window.location.origin + "/sign-up?ref=" + this.props.userInfo.referral_code : "";
-    const emailBody = "[Referral],\n\nI don't know if I've mentioned it before, but I've been working with Dane Chodos of Blackline Lending (www.blacklinelending.com) for commercial real estate loans. Given your financing needs, I thought you might also get a lot of value from their online lending platform.\n\n[Referral], meet Dane Chodos\nDane Chodos, meet [Referral]\n\nCan I leave the rest to you guys?\n\nTalk to you both later.";
+
     return (
       <div className="referral-section mt-20">
         <div className="row">
@@ -13,7 +14,9 @@ class ReferralLink extends Component {
               <h4>Your Unique Referral Link:</h4>
               <form>
                 <a href={link} target="_blank" style={{"color": "#0000EE", "fontSize": "14px"}}> Blackline Lending </a>
-                <a className="submit-btn-1 mt-5 pull-right" href={"mailto:?to=Referral Email&bcc=referral@blacklinelending.com&cc=dane.chodos@blacklinelending.com&subject=Check out Blackline Lending&body=" + escape(emailBody)}>Add Link in Your Email</a>
+                <ClipboardButton className="submit-btn-1 mt-5 pull-right" data-clipboard-text={link}>
+                  Copy to Clipboard
+                </ClipboardButton>
               </form>
             </div>
           </div>
--- client/app/components/pages/home/About.jsx
+++ client/app/components/pages/home/About.jsx
@@ -35,7 +35,7 @@ class About extends Component {
             “Ninety percent of all millionaires become so through owning real estate. More money has been made in real estate than in all industrial investments combined. The wise young man or wage earner of today invests his money in real estate.” -- Andrew Carnegie
           </h3>
         </div>
-        <div className="our-team mt-30">
+        <div className="our-team mt-60">
           <div className="about-sheltek-area ptb-90">
             <div className="container text-center">
               <div className="col-md-8 col-md-offset-2">
